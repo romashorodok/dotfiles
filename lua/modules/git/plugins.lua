6d8e9fda18ef3a2dd31bf2104402ab15e11fe12d
romashorodok <108732029+romashorodok@users.noreply.github.com>
2023-11-27 16:40:12 +0200

    add git merge conflict tool

--- lua/modules/git/plugins.lua
+++ lua/modules/git/plugins.lua
@@ -4,11 +4,28 @@ return function(use)
         config = function()
             local lzgit = require("lazygit")
 
-            vim.keymap.set("n", "<leader>g", lzgit.lazygit)
+            vim.keymap.set("n", "<leader>gg", lzgit.lazygit)
         end,
         lazy = false,
         dependencies = {
             "nvim-lua/plenary.nvim",
+        },
+    }
+
+    use
+    {
+        'akinsho/git-conflict.nvim',
+        event = 'VeryLazy',
+        opts = {
+            default_mappings = {
+                ours = '<leader>g1',
+                theirs = '<leader>g2',
+                none = '<leader>g0',
+                both = '<leader>g3',
+                prev = '[g',
+                next = ']g',
+            },
+            disable_diagnostics = true,
         },
     }
 end
