4a38da29fb9cfe9160aeaa1848f18cc324ac94c8
romashorodok <108732029+romashorodok@users.noreply.github.com>
2024-10-21 04:06:48 +0300

    feat: Use both

--- lua/modules/completion/lsp.lua
+++ lua/modules/completion/lsp.lua
@@ -234,26 +234,62 @@ local function default_handlers()
         }
     }
 
+    local function add_ruby_deps_command(client, bufnr)
+        vim.api.nvim_buf_create_user_command(bufnr, "ShowRubyDeps", function(opts)
+                local params = vim.lsp.util.make_text_document_params()
+                local showAll = opts.args == "all"
+
+                client.request("rubyLsp/workspace/dependencies", params, function(error, result)
+                    if error then
+                        print("Error showing deps: " .. error)
+                        return
+                    end
+
+                    local qf_list = {}
+                    for _, item in ipairs(result) do
+                        if showAll or item.dependency then
+                            table.insert(qf_list, {
+                                text = string.format("%s (%s) - %s", item.name, item.version, item.dependency),
+                                filename = item.path
+                            })
+                        end
+                    end
+
+                    vim.fn.setqflist(qf_list)
+                    vim.cmd('copen')
+                end, bufnr)
+            end,
+            { nargs = "?", complete = function() return { "all" } end })
+    end
+
 
     lspconfig.ruby_lsp.setup {
+        cmd = { "bundle", "exec", "ruby-lsp" },
+        init_options = {
+            formatter = 'standard',
+            linters = { 'standard' },
+        },
         capabilities = capabilities,
-        on_attach = default_on_attach,
+        on_attach = function(client, bufnr)
+            default_on_attach(client, bufnr)
+            add_ruby_deps_command(client, bufnr)
+        end
     }
 
 
-    -- lspconfig.solargraph.setup {
-    --     capabilities = capabilities,
-    --     on_attach = default_on_attach,
-    --     cmd = { "bundle", "exec", "solargraph", "stdio" },
-    --     init_options = {
-    --         completion = true,
-    --         diagnostic = true,
-    --         folding = true,
-    --         references = true,
-    --         rename = true,
-    --         symbols = true
-    --     },
-    -- }
+    lspconfig.solargraph.setup {
+        capabilities = capabilities,
+        on_attach = default_on_attach,
+        cmd = { "bundle", "exec", "solargraph", "stdio" },
+        init_options = {
+            completion = true,
+            diagnostic = true,
+            folding = true,
+            references = true,
+            rename = true,
+            symbols = true
+        },
+    }
 
     -- TODO: not works
     -- lspconfig.docker_compose_language_service.setup({
